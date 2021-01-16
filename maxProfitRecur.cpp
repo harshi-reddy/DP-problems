a48d973ece64ad059b8262d58236352f9dad3ffb
harshi-reddy <70446001+harshi-reddy@users.noreply.github.com>
2021-01-16 13:27:04 +0530

    Create maxProfitRecur.cpp
    Wine Problem using Recursion.
    Time Complexity : O(2^n)

---
+++ maxProfitRecur.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int maxProfit(int arr[], int be, int en, int yr) {
+    if(be > en) return 0;
+    int q1 = arr[be] * yr + maxProfit(arr, be + 1, en, yr + 1);
+    int q2 = arr[en] * yr + maxProfit(arr, be, en - 1, yr + 1);
+    return max(q1, q2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    cout << maxProfit(arr, 0, n-1, 1);
+}
